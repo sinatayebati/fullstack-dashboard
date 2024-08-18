bb64110d8dbe7afd43317560d0385c986ad3e68d
sinatayebati <sinatayebati@gmail.com>
2024-08-17 23:48:55 -0500

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import createTheme from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
-import Dashboard from "scene/dashboard";
+import Dashboard from "scenes/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
