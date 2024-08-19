b54b4e3785f0a632b478695b47dbafee81d7d48f
sinatayebati <sinatayebati@gmail.com>
2024-08-19 14:47:37 -0500

    sidebar development

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -49,7 +49,41 @@ const Sidebar = ({
     }, [pathname])
 
     return <Box component="nav">
-
+        {isSidebarOpen && (
+            <Drawer
+                open={isSidebarOpen}
+                onClose={() => setIsSidebarOpen(false)}
+                variant='persistent'
+                anchor='left'
+                sx={{
+                    width: drawerWidth,
+                    "& .MuiDrawer-paper": {
+                        color: theme.palette.secondary[200],
+                        backgroundColor: theme.palette.background.alt,
+                        boxSixing: "border-box",
+                        borderWidth: isNonMobile ? 0 : "2px",
+                        width: drawerWidth
+                    }
+                }}
+            >
+                <Box width="100%">
+                    <Box m="1.5rem 2rem 2rem 3rem">
+                        <FlexBetween color={theme.palette.secondary.main}>
+                            <Box display="flex" alignItems="center" gap="0.5rem">
+                                <Typography variant="h4" fontWeight="bold">
+                                    DASH.AI
+                                </Typography>
+                            </Box>
+                            {!isNonMobile && (
+                                <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                                    <ChevronLeft />
+                                </IconButton>
+                            )}
+                        </FlexBetween>
+                    </Box>
+                </Box>
+            </Drawer>
+        )}
     </Box>
 }
 
