4bd9008c8d5cb9894a024b93897c4dc3dba05065
sinatayebati <sinatayebati@gmail.com>
2024-08-19 00:51:05 -0500

    sidebar box

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import {
+    Box,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    listItemText,
+    Typography,
+    useTheme
+} from "@mui/material";
+import {
+    SettingsOutlined,
+    ChevronLeft,
+    ChevronRightOutlined,
+    HomeOutlined,
+    ShoppingCartOutlined,
+    Groups2Outlined,
+    ReceiptLongOutlined,
+    PublicOutlined,
+    PointOfSaleOutlined,
+    TodayOutlined,
+    CalendarMonthOutlined,
+    AdminPanelSettingsOutlined,
+    TrendingUpOutlined,
+    PieChartOutlined,
+} from "@mui/icons-material";
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import FlexBetween from './FlexBetween';
+import profileImage from "assets/profile.jpeg"
 
-const Sidebar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Sidebar = ({
+    drawerWidth,
+    isSidebarOpen,
+    setIsSidebarOpen,
+    isNonMobile,
+}) => {
+    const { pathname } = useLocation();
+    const [active, setActive] = useState("");
+    const navigate = useNavigate();
+    const theme = useTheme();
+
+    useEffect(() => {
+        setActive(pathname.substring(1));
+    }, [pathname])
+
+    return <Box component="nav">
+
+    </Box>
 }
 
 export default Sidebar
