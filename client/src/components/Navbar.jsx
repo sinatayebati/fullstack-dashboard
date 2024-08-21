175388b496aa8251e3e14b6c229c42ac897542d6
sinatayebati <sinatayebati@gmail.com>
2024-08-20 20:55:37 -0500

    state to open/close sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,10 @@ import {
 } from '@mui/material';
 
 
-const Navbar = () => {
+const Navbar = ({
+    isSidebarOpen,
+    setIsSidebarOpen,
+}) => {
     const dispatch = useDispatch();
     const theme = useTheme();
 
@@ -35,7 +38,7 @@ const Navbar = () => {
         <Toolbar sx={{ justifyContent: "space-between" }}>
             {/* LEFT SIDE */}
             <FlexBetween>
-                <IconButton onClick={() => console.log('open/close sidebar')}>
+                <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                     <MenuIcon />
                 </IconButton>
                 <FlexBetween
