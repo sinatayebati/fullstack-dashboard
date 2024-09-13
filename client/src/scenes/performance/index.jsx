b966de72e99cf789743041a1571f1c3ce85dcf76
sinatayebati <sinatayebati@gmail.com>
2024-09-13 00:38:21 -0500

    building the performance scene

--- client/src/scenes/performance/index.jsx
+++ client/src/scenes/performance/index.jsx
@@ -1,10 +1,104 @@
-import React from 'react'
+import React from 'react';
+import { Box, useTheme } from '@mui/material';
+import { useGetUserPerformanceQuery } from 'state/api';
+import { useSelector } from 'react-redux';
+import { DataGrid } from '@mui/x-data-grid';
+import Header from 'components/Header';
+import CustomColumnMenu from "components/DataGridCustomColumnMenu";
 
 const Performance = () => {
+  const theme = useTheme();
+  const userId = useSelector((state) => state.global.userId)
+  const { data, isLoading } = useGetUserPerformanceQuery(userId);
+  console.log("🚀 ~ Performance ~ data:", data)
+
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        if (params.value) {
+          return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+        }
+        return "N/A";
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
   return (
-    <div>
-      Performance
-    </div>
+    <Box m="1.5rem 2.5rem">
+        <Header title="PERFORMANCE" subTitle="Track your affiliate sales performance" />
+        {/* <Box
+            mt="40px"
+            height="75vh"
+            sx={{
+              "& .MuiDataGrid-root": {
+                border: "none",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: "none",
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: theme.palette.background.alt,
+                color: theme.palette.secondary[100],
+                borderBottom: "none",
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor: theme.palette.primary.light,
+              },
+              "& .MuiDataGrid-footerContainer": {
+                backgroundColor: theme.palette.background.alt,
+                color: theme.palette.secondary[100],
+                borderTop: "none",
+              },
+              "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                color: `${theme.palette.secondary[200]} !important`,
+              },
+            }}
+        >
+            <DataGrid
+                loading={isLoading || !data}
+                getRowId={(row) => row._id}
+                rows={data || []}
+                columns={columns}
+                components={{
+                  ColumnMenu: CustomColumnMenu,
+                }}
+            />
+        </Box> */}
+    </Box>
   )
 }
 
