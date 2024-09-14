90cb979b85d7085e1eadd237bf7eb64833e7fc1d
sinatayebati <sinatayebati@gmail.com>
2024-09-13 23:00:53 -0500

    setting up dashboard scene with api call to fetch data

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React from 'react';
+import FlexBetween from 'components/FlexBetween';
+import Header from 'components/Header';
+import { 
+  DownloadOutlined,
+  Email,
+  PointOfScale,
+  PersonAdd,
+  Traffic
+} from '@mui/icons-material';
+import { 
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import BreakdownChart from 'components/BreakdownChart';
+import OverviewChart from 'components/OverviewChart';
+import { useGetDashboardQuery } from 'state/api';
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px");
+  const { data, isLoading } = useGetDashboardQuery();
+  console.log("🚀 ~ Dashboard ~ data:", data)
+
   return (
     <div>Dashboard</div>
   )
