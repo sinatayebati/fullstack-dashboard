b7ee2918e20ccc9ea62f5c85d97f58965a4fd423
sinatayebati <sinatayebati@gmail.com>
2024-09-14 20:57:48 -0500

    updated overview chart with area under curve for dashbaord

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -95,6 +95,7 @@ const OverviewChart = ({ isDashboard = false, view }) => {
         }}
         yFormat=" >-.2f"
         curve='catmullRom'
+        enableArea={isDashboard}
         axisTop={null}
         axisRight={null}
         axisBottom={{
@@ -111,6 +112,8 @@ const OverviewChart = ({ isDashboard = false, view }) => {
             truncateTickAt: 0
         }}
         axisLeft={{
+            orient: "left",
+            tickValues: 5,
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
